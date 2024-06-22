07794efd3f31f9cbe0b981a63d90d364ae0e33a2
Gunjan Kumar <118360371+Gunjanraj321@users.noreply.github.com>
2024-06-22 21:40:26 +0530

    pollcontroller updated with result and route added

--- backend/controllers/pollController.js
+++ backend/controllers/pollController.js
@@ -14,4 +14,27 @@ const createPoll = async (req, res) =>{
     }
 }
 
-module.exports = {createPoll};
+const getPollResults = async (req, res) => {
+    const {pollId} = req.params;
+    try{
+        const poll = await Poll.findById(pollId);
+        if(!poll){
+            return res.status(404).json({message: 'Poll not found'});   
+            }
+        const pollResult = {
+            _id:poll._id,
+            question: poll.question,
+            options: poll.options.map(option=> ({
+                id:option.id,
+                text:option.text,
+                voteCount:option.voteCount,
+        })),
+        totalVotes: poll.options.reduce((total, option)=> total+ option.voteCount, 0)
+    }
+    return res.status(200).json(pollResult);
+}catch(err){
+    return res.status(400).json({message: err.message});
+}
+}
+
+module.exports = {createPoll, getPollResults};
--- backend/router/pollRouter.js
+++ backend/router/pollRouter.js
@@ -2,9 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 const {verify} = require('../middleware/verifyToken');
-const { createPoll } = require('../controllers/pollController');
+const { createPoll, getPollResults } = require('../controllers/pollController');
 console.log("verify:", typeof verify);
 
 router.post('/poll/createPoll',verify, createPoll);
+router.post('/poll/:pollId/pollResult', getPollResults);
 
 module.exports = router;
